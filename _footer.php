1dcb856ffc91115920a53983e3609bf0ab1dca64
Sivamohan Reddy Marthala <93118352+94siva@users.noreply.github.com>
2022-04-14 11:10:12 +0530

    Add files via upload

---
+++ _footer.php
@@ -0,0 +1,10 @@
+<footer class="foter">
+            <b>Made By: <a href="http://BDManagement.in">sivamohanredy</a></b>
+            <br>
+            &COPY;BDManagement
+</footer>
+
+
+
+</body>
+</html>
